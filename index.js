b8e85f28fd75869be435e7def2a9bd9da7c00221
unknown <julianatonu@gmail.com>
2022-10-25 11:15:51 +0600

    added req for tutorials

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 
 const categories = require('./data/categories.json')
+const tutorials=require('./data/tutorials.json')
 
 app.get('/', (req,res)=>{
     res.send('learn online server is running')
@@ -20,6 +21,16 @@ app.get('/categories/:id',(req, res)=>{
     const id = req.params.id;
     const category= categories.find(ct => ct.id === id)
     res.send(category)
+})
+
+app.get('/tutorials', (req, res)=>{
+    res.send(tutorials)
+})
+
+app.get('/tutorials/:id', (req, res)=>{
+    const id = req.params.id
+    const s_tutorials = tutorials.find(t => t._id === id)
+    res.send(s_tutorials)
 })
 
 app.listen(port, ()=>{
